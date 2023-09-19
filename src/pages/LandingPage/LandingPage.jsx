5ac8b80b3daee1f7ce44b61e456a7b5992675094
FlorinCP <florincosmin.peana@ulbsibiu.ro>
2023-09-19 23:40:29 +0300

    the form is almost finished

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./LandingPage.module.css";
 
 function LandingPage(props) {
@@ -7,16 +7,97 @@ function LandingPage(props) {
     phone: "",
     questionText: "",
   });
-
   const [nextPage, setNextPage] = useState(false);
+  const [selectedFiles, setSelectedFiles] = useState(null);
+  const [selectedFilesObj, setSelectedFilesObj] = useState(null);
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = (event) => {
+    setIsChecked(event.target.checked);
+  };
+
+  const handleFileChange = (e) => {
+    const filesArray = Array.from(e.target.files);
+    setSelectedFiles(filesArray);
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setQuestionData({ ...questionData, [name]: value });
   };
 
+  useEffect(() => {
+    if (selectedFiles) {
+      const obj = [];
+
+      selectedFiles.forEach((file, index) => {
+        let type = file.type;
+        const indexOfSlash = type.indexOf("/");
+        const finalType = type.substring(indexOfSlash + 1);
+
+        const img = new Image();
+
+        switch (finalType) {
+          case "pdf":
+            img.src = "/pdf-icon.svg";
+            break;
+          case "jpg":
+          case "png":
+          case "jpeg":
+            img.src = "/img-icon.svg";
+            break;
+          case "doc":
+            img.src = "/word-img.svg";
+            break;
+        }
+
+        let name = file.name;
+        const indexOfDot = name.indexOf(".");
+        const finalName = name.substring(0, indexOfDot);
+
+        let fileSize = file.size;
+        const finalSize = fileSize / 1024 ** 2;
+
+        obj.push({
+          image: img,
+          type: finalType,
+          name: finalName,
+          size: finalSize.toFixed(2),
+        });
+      });
+
+      console.log(obj);
+      setSelectedFilesObj(obj);
+    }
+  }, [selectedFiles]);
+
+  const handleUpload = () => {
+    const formData = new FormData();
+
+    selectedFiles.forEach((file, index) => {
+      formData.append(`files`, file);
+    });
+
+    console.log(formData);
+
+    fetch("http://localhost:8080/cjc/api/v1/file/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response from the Spring Boot backend
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error uploading file:", error);
+      });
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    handleUpload();
 
     try {
       const response = await fetch(
@@ -40,6 +121,12 @@ function LandingPage(props) {
       console.error("Error sending data:", error);
     }
   };
+
+  function moveForward(e) {
+    e.preventDefault();
+    console.log("something");
+    setNextPage((prevState) => !prevState);
+  }
 
   return (
     <div className={style.wrapper}>
@@ -104,65 +191,173 @@ function LandingPage(props) {
           <p className={style.title}>Formular intrebari</p>
         </div>
 
-        {!nextPage && (
-          <div className={style.formPage}>
-            <div className={style.pageIndex}>
-              <div id={style["line"]}></div>
-              <div className={style.holly}>
-                <button className={style.indexButton}>1</button>
-                Fisiere si Descriere
-              </div>
-              <div className={style.holly}>
-                <button className={style.indexButton}>2</button>
-                Creeare Cont
-              </div>
+        <div className={style.formPage}>
+          <div className={style.pageIndex}>
+            <div id={nextPage ? style["line"] : style["lineDisabled"]}></div>
+            <div className={style.holly}>
+              <button className={style.indexButton}>1</button>
+              Fisiere si Descriere
             </div>
-
-            <h2>Textul intrebarii</h2>
-
-            <textarea
-              name="questionText"
-              value={questionData.questionText}
-              onChange={handleChange}
-              className={style.enterQuestion}
-              cols="20"
-              rows="7"
-            ></textarea>
-
-            <button type="submit" className={style.actionBtn}>
-              Urmatorul Pas
-              <span className="material-symbols-outlined">chevron_right</span>
-            </button>
-          </div>
-        )}
-
-        {nextPage && (
-          <div className={style.formPage}>
-            <div id={style["inputs"]}>
-              <input
-                type="text"
-                name="phone"
-                value={questionData.phone}
-                onChange={handleChange}
-                className={style.inputField}
-                placeholder="Phone"
-              />
-
-              <input
-                type="text"
-                name="email"
-                value={questionData.email}
-                onChange={handleChange}
-                className={style.inputField}
-                placeholder="Email"
-              />
+            <div div className={nextPage ? style.holly : style.hollyDisabled}>
+              <button className={style.indexButton}>2</button>
+              Creeare Cont
             </div>
-
-            <button type="submit" className={style.actionBtn}>
-              Trimite intrebarea !
-            </button>
           </div>
-        )}
+
+          {!nextPage ? (
+            <>
+              <h2>Textul intrebarii</h2>
+
+              <textarea
+                name="questionText"
+                value={questionData.questionText}
+                onChange={handleChange}
+                className={style.enterQuestion}
+                cols="20"
+                rows="7"
+              ></textarea>
+
+              <h2>Incarcarea Fisierelor</h2>
+              <div className={style.fileUpload}>
+                <div id={style["fileHeader"]}>
+                  <label
+                    htmlFor="file-upload"
+                    className={style.customFileUpload}
+                  >
+                    <span className="material-symbols-outlined">
+                      upload_file
+                    </span>{" "}
+                    <span>Alegeti fisierele</span>
+                  </label>
+                  <input
+                    type="file"
+                    id="file-upload"
+                    onChange={handleFileChange}
+                    multiple
+                  />
+                  {/*<button onClick={handleUpload}>Incarcati</button>*/}
+                </div>
+
+                <div id={style["uploadedFiles"]}>
+                  {selectedFilesObj ? (
+                    selectedFilesObj.map((file, index) => (
+                      <div className={style.fileRepresentation} key={index}>
+                        <img src={file.image.src} alt="" />
+                        <p>{file.name}</p>
+                        <h4>{file.size} MB</h4>
+
+                        {/* fff util pt descarcare */}
+                        {/*<a*/}
+                        {/*  href={URL.createObjectURL(file)}*/}
+                        {/*  download={file.name}*/}
+                        {/*>*/}
+                        {/*  Download*/}
+                        {/*</a>*/}
+                      </div>
+                    ))
+                  ) : (
+                    <></>
+                  )}
+                </div>
+              </div>
+
+              <button className={style.actionBtn} onClick={moveForward}>
+                Urmatorul Pas
+                <span className="material-symbols-outlined">chevron_right</span>
+              </button>
+            </>
+          ) : (
+            <>
+              <div id={style["inputs"]}>
+                <div id={style["swichDiv"]}>
+                  <p className={!isChecked ? style.choiceP : style.choiceD}>
+                    Cont nou
+                  </p>
+                  <label className={style.switch}>
+                    <input
+                      type="checkbox"
+                      checked={isChecked}
+                      onChange={handleCheckboxChange}
+                    ></input>
+                    <span className={`${style.slider} ${style.round}`}></span>
+                  </label>
+                  <p className={isChecked ? style.choiceP : style.choiceD}>
+                    Autentificare
+                  </p>
+                </div>
+
+                {isChecked ? (
+                  <>
+                    <h3>Email / Numar de telefon</h3>
+                    <input
+                      type="text"
+                      name="email"
+                      value={questionData.email}
+                      onChange={handleChange}
+                      className={style.inputField}
+                    />
+
+                    <h3>Parola</h3>
+                    <input
+                      type="password"
+                      name="password"
+                      value={questionData.password}
+                      onChange={handleChange}
+                      className={style.inputField}
+                    />
+                  </>
+                ) : (
+                  <>
+                    <h3>Numar de telefon</h3>
+                    <input
+                      type="text"
+                      name="phone"
+                      value={questionData.phone}
+                      onChange={handleChange}
+                      className={style.inputField}
+                    />
+
+                    <h3>Email</h3>
+                    <input
+                      type="text"
+                      name="email"
+                      value={questionData.email}
+                      onChange={handleChange}
+                      className={style.inputField}
+                    />
+
+                    <h3>Parola</h3>
+                    <input
+                      type="password"
+                      name="password"
+                      value={questionData.password}
+                      onChange={handleChange}
+                      className={style.inputField}
+                    />
+
+                    <h3>Confirmare Parola</h3>
+                    <input
+                      type="password"
+                      name="password"
+                      value={questionData.confirmPassword}
+                      onChange={handleChange}
+                      className={style.inputField}
+                    />
+                  </>
+                )}
+              </div>
+
+              <div className={style.buttons}>
+                <button className={style.actionBtn2} onClick={moveForward}>
+                  Inapoi
+                </button>
+                <button type="submit" className={style.actionBtn2}>
+                  Trimite intrebarea !
+                </button>
+              </div>
+            </>
+          )}
+        </div>
       </form>
 
       <div className={style.footer}>
